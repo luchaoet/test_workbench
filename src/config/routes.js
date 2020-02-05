cc3889b193cee4276b7d0284607dfe639e0f42ba
Lucy20209060 <550502661@qq.com>
2020-02-05 23:30:01 +0800

    新增页面

--- src/config/routes.js
+++ src/config/routes.js
@@ -4,6 +4,7 @@ import NotFound from '@/pages/NotFound';
 import Home from '@/pages/Home';
 import Management from '@/pages/Management';
 import List from '@/pages/List';
+import App from '@/pages/App';
 
 const routerConfig = [
   {
@@ -14,6 +15,7 @@ const routerConfig = [
       {path: '/home', component: Home},
       {path: '/list', component: List},
       {path: '/management', component: Management},
+      {path: '/app', component: App},
       {component: NotFound},
     ],
   },
---
+++ src/pages/App/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './index.module.scss';
+
+export default function AppPage() {
+  return (
+    <div className={styles.wrap}>
+      <div>App</div>
+    </div>
+  );
+}
